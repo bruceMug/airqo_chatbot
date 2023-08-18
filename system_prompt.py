be8583b3667a216b198fe2ef32f684c5a32ba50a
brucemug <mugzbru199@gmail.com>
2023-08-18 11:08:52 +0300

    initialised system_prompt

---
+++ system_prompt.py
@@ -0,0 +1,5 @@
+system_prompt = """
+    ```
+    Remember don't provide an answer outside of the information provided. Politely! give the answer as {I don't have access to that information} only. 
+    Another reminder: Keep your responses as short as possible and simple for people to understand.
+    """
